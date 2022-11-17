d4aeb8a2736ef23b5412c0a75b99dc04b112fe54
kingluo <home_king@163.com>
2022-11-18 00:53:32 +0800

    python: merge cffi

--- examples/python/echo.py
+++ examples/python/echo.py
@@ -1,4 +1,12 @@
-from nffi import *
+from cffi import FFI
+ffi = FFI()
+ffi.cdef("""
+char *strdup(const char *s);
+void* ngx_http_lua_nonblocking_ffi_task_poll(void *p);
+char* ngx_http_lua_nonblocking_ffi_get_req(void *tsk, int *len);
+void ngx_http_lua_nonblocking_ffi_respond(void *tsk, int rc, char* rsp, int rsp_len);
+""")
+C = ffi.dlopen(None)
 
 import threading
 
--- examples/python/ffi/kafka.py
+++ examples/python/ffi/kafka.py
@@ -1,4 +1,13 @@
-from nffi import *
+from cffi import FFI
+ffi = FFI()
+ffi.cdef("""
+void* malloc(size_t);
+void *memcpy(void *dest, const void *src, size_t n);
+void* ngx_http_lua_nonblocking_ffi_task_poll(void *p);
+char* ngx_http_lua_nonblocking_ffi_get_req(void *tsk, int *len);
+void ngx_http_lua_nonblocking_ffi_respond(void *tsk, int rc, char* rsp, int rsp_len);
+""")
+C = ffi.dlopen(None)
 
 import threading
 import json
