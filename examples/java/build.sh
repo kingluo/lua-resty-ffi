d60be1083bf553e444f35931743bb9e1ad80d2db
kingluo <home_king@163.com>
2022-11-22 22:04:10 +0800

    fix

--- examples/java/build.sh
+++ examples/java/build.sh
@@ -12,10 +12,8 @@ if [[ $ID == "centos" ]]; then
         source /opt/rh/gcc-toolset-9/enable
     fi
     set -eu
-    yum -y install java-11-openjdk-devel
     gcc -O2 -shared -fPIC -o libffi_java.so loader.c -I/usr/lib/jvm/java-11-openjdk/include/ -I/usr/lib/jvm/java-11-openjdk/include/linux/ -L/usr/lib/jvm/java-11-openjdk/lib/ -L/usr/lib/jvm/java-11-openjdk/lib/server/ -ljvm -pthread
 else
-    apt -y install openjdk-11-jdk
     gcc -O2 -shared -fPIC -o libffi_java.so loader.c -I/usr/lib/jvm/java-11-openjdk-amd64/include/ -I/usr/lib/jvm/java-11-openjdk-amd64/include/linux/ -L/usr/lib/jvm/java-11-openjdk-amd64/lib/ -L/usr/lib/jvm/java-11-openjdk-amd64/lib/server/ -ljvm -pthread
 fi
 
--- examples/python/build.sh
+++ examples/python/build.sh
@@ -12,16 +12,13 @@ if [[ $ID == "centos" ]]; then
         source /opt/rh/gcc-toolset-9/enable
     fi
     set -eu
-    yum -y install python3-devel
     gcc $(python3-config --cflags) $(python3-config --ldflags) -o libffi_python3.so loader.c \
         -fPIC -shared -pthread
 elif [[ $ID == "ubuntu" ]]; then
-    apt -y install python3-dev python3-pip libffi-dev
     gcc $(python3-config --cflags) $(python3-config --ldflags) -o libffi_python3.so loader.c \
         -lpython$(python3 -c 'import sys; print(".".join(map(str, sys.version_info[:2])))') \
         -fPIC -shared -pthread
 elif [[ $ID == "debian" ]]; then
-    apt -y install python3-dev python3-pip libffi-dev
     gcc $(python3-config --cflags) $(python3-config --ldflags) -o libffi_python3.so loader.c \
         -fPIC -shared -pthread
 else
