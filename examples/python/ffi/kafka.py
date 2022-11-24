5606351fa90152cdaeac9c9089e04510cba3bc3d
kingluo <home_king@163.com>
2022-11-24 15:01:25 +0800

    python: kafka: fix topic create and consume

--- examples/python/ffi/kafka.py
+++ examples/python/ffi/kafka.py
@@ -12,43 +12,54 @@ C = ffi.dlopen(None)
 import threading
 import json
 import queue
+import time
 from kafka import KafkaProducer
 from kafka import KafkaConsumer
+from kafka import KafkaAdminClient
+from kafka.admin import NewTopic
+from kafka.errors import TopicAlreadyExistsError
 
 class State:
     def __init__(self, cfg):
-        self.taskq = queue.Queue()
-
         self.topic = cfg["topic"]
         servers = cfg["servers"]
+
+        self.taskq = queue.Queue()
+        self.consume_taskq = queue.Queue()
+
         self.producer = KafkaProducer(bootstrap_servers=servers)
-        self.consumer = KafkaConsumer(bootstrap_servers=servers, group_id=cfg["group_id"], enable_auto_commit=True)
+        self.consumer = KafkaConsumer(bootstrap_servers = servers, group_id = cfg["group_id"], enable_auto_commit = True)
         self.consumer.subscribe([self.topic])
 
-        self.msgs = []
-        self.consume_tasks = []
-        self.mutex = threading.Lock()
+        try:
+            admin = KafkaAdminClient(bootstrap_servers=servers)
+            topic_list = []
+            topic_list.append(NewTopic(name=self.topic, num_partitions=1, replication_factor=1))
+            print(admin.create_topics(new_topics=topic_list, validate_only=False))
+            msgs = self.consumer.poll()
+            assert not msgs
+        except TopicAlreadyExistsError as err:
+            pass
 
-        t = threading.Thread(target=self.consume)
+        t = threading.Thread(target=self.consumer_loop)
         t.daemon = True
         t.start()
         self.consumer_thread = t
 
-        t = threading.Thread(target=self.task_loop)
+        t = threading.Thread(target=self.producer_loop)
         t.daemon = True
         t.start()
         self.task_loop_thread = t
-
-    def produce(self, msg):
-        self.producer.send(self.topic, msg.encode())
 
     def respond(self, task, data):
         res = C.malloc(len(data))
         C.memcpy(res, data.encode(), len(data))
         C.ngx_http_lua_ffi_respond(task, 0, res, len(data))
 
-    def consume(self):
-        for msg in self.consumer:
+    def consumer_loop(self):
+        while True:
+            task, _ = self.consume_taskq.get()
+            msg = next(self.consumer)
             ev = {
                 "topic": msg.topic,
                 "partition": msg.partition,
@@ -56,46 +67,32 @@ class State:
                 "key": msg.key,
                 "value": msg.value.decode(),
             }
-            self.mutex.acquire()
-            try:
-                if not self.consume_tasks:
-                    self.msgs.append(ev)
-                else:
-                    data = json.dumps(ev)
-                    for task in self.consume_tasks:
-                        self.respond(task, data)
-                    self.consume_tasks.clear()
-            finally:
-                self.mutex.release()
+            data = json.dumps(ev)
+            self.respond(task, data)
+            self.consume_taskq.task_done()
 
-    def task_loop(self):
+    def producer_loop(self):
         while True:
             task, req = self.taskq.get()
-            req = json.loads(ffi.string(req))
-            typ = req["type"]
-            if typ == "produce":
-                self.produce(req["msg"])
-                C.ngx_http_lua_ffi_respond(task, 0, ffi.NULL, 0)
-            elif typ == "consume":
-                self.mutex.acquire()
-                try:
-                    if self.msgs:
-                        data = json.dumps(self.msgs)
-                        self.msgs.clear()
-                        self.respond(task, data)
-                    else:
-                        self.consume_tasks.append(task)
-                finally:
-                    self.mutex.release()
-            else:
-                C.ngx_http_lua_ffi_respond(task, -1, ffi.NULL, 0)
+            msg = req["msg"]
+            future = self.producer.send(self.topic, msg.encode())
+            result = future.get(timeout=60)
+            print(result, msg)
+            C.ngx_http_lua_ffi_respond(task, 0, ffi.NULL, 0)
             self.taskq.task_done()
 
     def poll(self, tq):
         while True:
             task = C.ngx_http_lua_ffi_task_poll(ffi.cast("void*", tq))
             r = C.ngx_http_lua_ffi_get_req(task, ffi.NULL)
-            self.taskq.put((task, r))
+            req = json.loads(ffi.string(r))
+            typ = req["type"]
+            if typ == "produce":
+                self.taskq.put((task, req))
+            elif typ == "consume":
+                self.consume_taskq.put((task, req))
+            else:
+                C.ngx_http_lua_ffi_respond(task, -1, ffi.NULL, 0)
 
 def init(cfg, tq):
     data = ffi.string(ffi.cast("char*", cfg))
